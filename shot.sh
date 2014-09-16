4f3b2930834d60fea883009c17b721c91e8e49ad
Kouga <ncwhale@gmail.com>
2014-09-16 13:41:59 +0800

    A screen shot script added.

--- shot.sh
+++ shot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-SPATH="$HOME/9@kouga.us/$(coffee -e 't=new Date();console.log t.getFullYear()')临时截图"
+SPATH="$HOME/Shots/$(coffee -e 't=new Date();console.log t.getFullYear()')截图"
 FILENAME="$SPATH/$(python -c 'from datetime import datetime; print(datetime.strftime(datetime.now(), "%Y-%m-%d-%H%M%S-%f"))').png"
 
 mkdir -p -v $SPATH
 echo $FILENAME
 
-shutter -s -n -e -o "$FILENAME"
+shutter -s -n -e -o "$FILENAME" >/dev/null 2>&1
 curl -F "image=@$FILENAME" -F "key=5401e4c11aea4001d574f64b406c0359" https://imgur.com/api/upload.xml | grep -E -o "<original_image>(.)*</original_image>" | grep -E -o "http://i.imgur.com/[^<]*" | xclip -selection c
 notify-send "Screenshot has successfully uploaded to $(xclip -selection c -o)"
 
